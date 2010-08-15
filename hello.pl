102d68ecf3ea9228070b6ba720b3edc8b6184bae
kusemek <romsok.tech@gmail.com>
2010-08-15 09:48:41 -0400

    removed the "moved" files. they are all now in their respective directories

--- hello.pl
+++
@@ -1,9 +0,0 @@
-print "Hello World\n";
-
-$x = 10;
-
-@array = (1,2);
-
-print @array;
-print "\n";
-print $x;
--- sort_strings_3_3.pl
+++
@@ -1,9 +0,0 @@
-#!perl -w
-
-print("enter strings: \n");
-chomp(@strings = <STDIN>);
-@strings = sort(@strings);
-
-foreach(@strings) {
-	print("$_ ");
-}
